7465959fd3d1649af3cf192a2578021fb366deb5
Jan Hein de Jong <JHdJ@allseas.com>
2020-07-10 01:26:38 +0200

    Bedtime

--- scores.py
+++ scores.py
@@ -1,11 +1,8 @@
-import uuid
 import json
 from typing import NamedTuple
 import sqlite3
 import hashlib
 import time
-
-import pandas as pd
 
 
 WEIGHT_PERFORMANCE = 0.75  # Relative importance of winning versus participating
@@ -17,7 +14,7 @@ class AlreadyExists(Exception):
 
 
 class Game(NamedTuple):
-    game_id: str
+    id: str
     name: str
     datetime: str
     duration: int
@@ -35,14 +32,25 @@ def init_db():
     with sqlite3.connect(DATABASE_URL) as conn:
         conn.execute(
             """ 
-            CREATE TABLE IF NOT EXISTS games 
-            (game_id text, name text, datetime text, duration int, points int)
+            CREATE TABLE IF NOT EXISTS games (
+                id text, 
+                name text, 
+                datetime text, 
+                duration int, 
+                points int,
+                PRIMARY KEY (id)
+            )
             """
         )
         conn.execute(
             """ 
-            CREATE TABLE IF NOT EXISTS participations 
-            (game_id text, player text, rank int, score real)
+            CREATE TABLE IF NOT EXISTS participations (
+                game_id text, 
+                player text, 
+                rank int, 
+                score real,
+                FOREIGN KEY (game_id) REFERENCES games (id)
+            )
             """
         )
 
@@ -50,10 +58,8 @@ def init_db():
 def post_game(**kwargs):
     game, participations = _parse_game_dict(**kwargs)
     with sqlite3.connect(DATABASE_URL) as conn:
-        if conn.execute(
-            "SELECT 1 FROM games WHERE game_id = ?", (game.game_id,)
-        ).fetchone():
-            raise AlreadyExists("Entry already exists")
+        if conn.execute("SELECT 1 FROM games WHERE id = ?", (game.id,)).fetchone():
+            raise AlreadyExists(f"Game with id {game.id} already exists")
         conn.execute(
             """
             INSERT INTO 
@@ -66,7 +72,7 @@ def post_game(**kwargs):
         conn.executemany(
             """
             INSERT INTO 
-                participations 
+                participations
             VALUES 
                 (?, ?, ?, ?)
             """,
@@ -76,65 +82,69 @@ def post_game(**kwargs):
 
 def read_stuff():
     conn = sqlite3.connect(DATABASE_URL)
-    c = conn.execute("""SELECT * FROM games""")
+    c = conn.execute("""SELECT * FROM games """)
     for game in c:
         print(game)
 
 
 def get_scores():
-    conn = sqlite3.connect(DATABASE_URL)
-    TOTAL_POINTS = conn.execute("SELECT SUM(points) FROM games").fetchone()[0]
-    c = conn.execute(
-        """
-        SELECT 
-            player, 
-            ROUND(100 * performance, 1), 
-            ROUND(100 * participation, 1), 
-            ROUND(100 * (:weight * performance + (1 - :weight) * participation), 1) AS score
-        FROM 
-        (
+    with sqlite3.connect(DATABASE_URL) as conn:
+        TOTAL_POINTS = conn.execute("SELECT SUM(points) FROM games").fetchone()[0]
+        c = conn.execute(
+            """
             SELECT 
-                player AS player,
-                SUM(score) / SUM(points) AS performance,
-                SUM(points) / CAST(:total_points AS float) AS participation
+                player, 
+                ROUND(100 * performance, 1), 
+                ROUND(100 * participation, 1), 
+                ROUND(100 * (? * performance + ? * participation), 1) AS score
             FROM 
-                games 
-            INNER JOIN participations ON participations.game_id = games.game_id
-            GROUP BY 
-                player
+            (
+                SELECT 
+                    player AS player,
+                    SUM(score) / SUM(points) AS performance,
+                    SUM(points) / CAST(? AS float) AS participation
+                FROM 
+                    games 
+                INNER JOIN participations ON participations.game_id = games.id
+                GROUP BY 
+                    player
+            )
+            ORDER BY 
+                score
+            """,
+            (WEIGHT_PERFORMANCE, 1 - WEIGHT_PERFORMANCE, TOTAL_POINTS),
         )
-        ORDER BY 
-            score
-        """,
-        {"weight": WEIGHT_PERFORMANCE, "total_points": TOTAL_POINTS},
-    )
-    for player in c:
-        print(player)
+        for player in c:
+            print(
+                "{} won {}%, participated in {}% and has an overall score of {}%".format(
+                    *player
+                )
+            )
 
 
 def main():
     init_db()
     with open("scores.json") as f:
         for game in json.load(f):
-            game["game_id"] = _generate_game_id(**game)
+            game["id"] = _generate_game_id(**game)
             try:
                 post_game(**game)
-            except AlreadyExists:
-                print(f"Game with ID {game['game_id']} already exists!")
+            except AlreadyExists as err:
+                print(err)
     read_stuff()
     t = time.time()
     get_scores()
     print(f"This took {time.time() - t} seconds")
 
 
-def _parse_game_dict(game_id, name, datetime, duration, ranking):
+def _parse_game_dict(id, name, datetime, duration, ranking):
     points = _game_points(duration)
-    game = Game(game_id, name, datetime, duration, points)
+    game = Game(id, name, datetime, duration, points)
     participations = []
     max_rank = max(ranking.values())
     for player, rank in ranking.items():
         score = points * (max_rank - rank) / (max_rank - 1)
-        participations.append(Participation(game_id, player, rank, score))
+        participations.append(Participation(game.id, player, rank, score))
     return game, participations
 
 
@@ -156,20 +166,3 @@ def _game_points(duration):
 
 if __name__ == "__main__":
     main()
-
-
-# def main():
-#     games_df = pd.DataFrame()
-#     participations_df = pd.DataFrame()
-#     with open("scores.json") as f:
-#         for game in json.load(f):
-#             game, participations = _parse_game_dict(**game)
-#             games_df = games_df.append(pd.DataFrame([game]))
-#             participations_df = participations_df.append(pd.DataFrame(participations))
-#     df = pd.merge(games_df, participations_df, on="game_id")
-#     scores = 100 * (
-#         RATIO * df.groupby("player").score.sum() / df.groupby("player").points.sum()
-#         + (1 - RATIO) * df.groupby("player").points.sum() / games_df.points.sum()
-#     )
-#     print(scores.sort_values())
-#     print(games_df.duration.sum())
